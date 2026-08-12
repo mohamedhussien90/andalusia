a4372148bc7e9e44e4add41458eaa7a04d65bcbe
mohamedhussien90 <108836009+mohamedhussien90@users.noreply.github.com>
2026-08-12 22:00:40 +0200

    Add files via upload
    Assignment 3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using WebApplication2.Middleware;
+using WebApplication2.Repo;
+using WebApplication2.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddSingleton<ITaskRepo, TaskRepo>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+app.MapControllers();
+
+app.Run();
---
+++ Service/ITaskService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication2.Taskitem;
+
+namespace WebApplication2.Service
+{
+    public interface ITaskService
+    {
+        List<TaskItem> GetAll();
+        TaskItem GetById(int id);
+        TaskItem Create(TaskItem task);
+        TaskItem Update(int id, TaskItem task);
+        void Delete(int id);
+    }
+}
