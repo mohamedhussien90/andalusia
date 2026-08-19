e656db460105fcf0df123e17aeb7587b2958ce91
mohamedhussien90 <108836009+mohamedhussien90@users.noreply.github.com>
2026-08-19 21:50:49 +0200

    Add files via upload

---
+++ DataBASE/Models/TaskItem.cs
@@ -0,0 +1,14 @@
+﻿namespace DataBaseTesting.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        // Foreign Key
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ DataBASE/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBaseTesting.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
